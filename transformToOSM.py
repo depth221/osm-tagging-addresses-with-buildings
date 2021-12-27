dbbc93fd56bf7fbe4e9f44ab33d2efbd2179200d
Dongha Hwang <depth221@gmail.com>
2021-12-27 19:36:42 +0900

    add transformToOSM.py

--- transformToOSM.py
+++ transformToOSM.py
@@ -2,58 +2,61 @@ import csv
 import sys
 from xml.etree.ElementTree import Element, SubElement, ElementTree
 
-osm_file_path = sys.argv[1]
-csv_file_path = sys.argv[1][:-3] + "csv"
+#osm_file_path = sys.argv[1]
+#csv_file_path = sys.argv[1][:-3] + "csv"
 
-if not len(sys.argv) in [2, 3]: # if without a path of csv file to read
-    print("Usage: python csvtoosm.py <csv_file> (<incoding_of_csv_file>)\n")
-    sys.exit()
+#if not len(sys.argv) in [2, 3]: # if without a path of csv file to read
+#    print("Usage: python csvtoosm.py <csv_file> (<incoding_of_csv_file>)\n")
+#    sys.exit()
 
-csv_incoding = "utf-8" # set the incoding of csv file to read
-if len(sys.argv) == 3:
-    csv_incoding = sys.argv[2]
-print("Info: Incoding of the csv file is " + csv_incoding)
+csv_file_path = "test_address.csv"
+input_osm_file_path = "test.osm"
+output_osm_file_path = input_osm_file_path[:-4] + "_addr.osm"
 
-fr = open(csv_file_path, 'r', encoding=csv_incoding)
+input_csv_file = open(csv_file_path, 'r', encoding="utf-8")
+input_csv = csv.reader(input_csv_file)
 
-fr_csv_r = csv.reader(fr);
-fr_csv = []
-for line in fr_csv_r: # convert the csv object to a list
-    fr_csv.append(line)
+input_osm_file = open(input_osm_file_path, 'r', encoding="utf-8")
+input_xml = ElementTree()
+input_xml.parse(input_osm_file)
+root = input_xml.getroot()
 
-fw_xml = Element('osm') # root of osm file
-fw_xml.attrib['version'] = '0.6'
-fw_xml.attrib['generator'] = 'JOSM'
-
-keys_to_add = fr_csv[0]
-
-for i in range(1, len(fr_csv)):
-    node = SubElement(fw_xml, 'node') # add each node
-    node.attrib['id'] = str(-200000 - i)
-    node.attrib['action'] = 'modify'
-    node.attrib['visible'] = 'true'
-
-    if not fr_csv[i][0] or not fr_csv[i][1]:
-        print("Error: All of lines must have coordinate")
-        sys.exit()
-    else:    
-        node.attrib['lat'] = fr_csv[i][0]
-        node.attrib['lon'] = fr_csv[i][1]
-
-    for j in range(2, len(keys_to_add)): # add tags
-        if fr_csv[i][j]:
-            tag = SubElement(node, 'tag')
-            tag.attrib['k'] = keys_to_add[j]
-            tag.attrib['v'] = fr_csv[i][j]
-
-tree = ElementTree(fw_xml)
-print("Info: Done building the tree")
-try:
-    tree.write(osm_file_path, encoding = "utf-8", xml_declaration = True) # write
-except:
-    print("Error: Please delete '" + osm_file_path + "'")
-    sys.exit()
+for line in input_csv:
+    if line[0] == "id":
+        continue
     
-print("Info: Writed to '" + osm_file_path + "'")
+    for way in root.iter("way"):
+        if way.attrib["id"] == line[0]:
+            has_addr_street = False
+            has_addr_housenumber = False
+            
+            for tag in way.iter("tag"):
+                if tag.attrib["k"] == "addr:street":
+                    has_addr_street = True
 
-fr.close()
+            if has_addr_street:
+                continue
+
+            street_tag = SubElement(way, "tag")
+            street_tag.attrib["k"] = "addr:street"
+            street_tag.attrib["v"] = line[6]
+            way.attrib["action"] = "modify"
+
+
+            for tag in way.iter("tag"):
+                if tag.attrib["k"] == "addr:housenumber":
+                    has_addr_housenumber = True
+
+            if has_addr_housenumber:
+                continue
+
+            housenumber_tag = SubElement(way, "tag")
+            housenumber_tag.attrib["k"] = "addr:housenumber"
+            housenumber_tag.attrib["v"] = line[7]
+
+input_xml.write(output_osm_file_path)
+
+print("Info: Writed to '" + output_osm_file_path + "'")
+
+input_csv_file.close()
+input_osm_file.close()
